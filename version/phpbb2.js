eb01028f6ec6718ca81d58c08211e01887ef5870
SSYT 2.0 <SSYT@users.noreply.github.com>
2017-01-23 12:17:59 +0200

    Create phpbb2.js

---
+++ version/phpbb2.js
@@ -0,0 +1,9 @@
+/*
+  * FA Module configuration !
+  * Forum version: phpBB2
+*/
+var forumKeyURL = "forumcodes", forumKeyVersion = 1,
+    forumKeyLink = "/register", forumKeyName = "Link",
+    forumVersionName = "faModule.phpbb2.js";
+
+document.write('<script type="text/javascript" src="https://github.com/SSYT/FG-Modules/releases/download/fa_module_tr/'+ forumVersionName +'"><\/script>');
