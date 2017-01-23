8ef4a67cb40c535f7ec33a3fa17acdc2d6964a6e
SSYT 2.0 <SSYT@users.noreply.github.com>
2017-01-23 20:33:25 +0200

    Delete phpbb3.js

--- version/phpbb3.js
+++
@@ -1,8 +0,0 @@
-/*
-  * FA Module configuration !
-*/
-var forumKeyURL = "forumcodes", forumKeyVersion = 1,
-    forumKeyLink = "/register", forumKeyName = "Link",
-    forumVersionName = "faModule.phpbb3.js";
-
-document.write('<script type="text/javascript" src="https://github.com/SSYT/FG-Modules/releases/download/fa_module_tr/'+ forumVersionName +'"><\/script>');
