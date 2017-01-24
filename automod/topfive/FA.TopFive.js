1e505ad2ac736d629b59d390e0a4b21bc3b7f185
SSYT 2.0 <SSYT@users.noreply.github.com>
2017-01-24 12:10:49 +0200

    Update FA.TopFive.js

--- automod/topfive/FA.TopFive.js
+++ automod/topfive/FA.TopFive.js
@@ -4,9 +4,14 @@
 	config: {
           translate: {
             en: {
-              
+              'L_NEWEST_TOPICS'   : 'Newest Posts',
+              'L_TOP_FIVE_ACTIVE' : 'Top Active Users',
+              'L_TOP_FIVE_NEWEST' : 'Newest Users'
             },
             ro: {
+              'L_NEWEST_TOPICS'   : 'Cele mai noi mesaje',
+              'L_TOP_FIVE_ACTIVE' : 'Top utilizatori activi',
+              'L_TOP_FIVE_NEWEST' : 'Cele mai noi utilizatori'
             }
           }
 	},
@@ -14,7 +19,9 @@
 	options: {
           module: 3,
           autoMod: true,
-          moduleName: 'Top Five'
+          moduleName: 'Top Five',
+          version: 1,
+          language: 'en'
 	}
   };
   document.write('');
