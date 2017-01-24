9c479a0b175d00814d95dd6362d0d28996163906
SSYT 2.0 <SSYT@users.noreply.github.com>
2017-01-24 12:02:43 +0200

    Create FA.TopFive.js

---
+++ automod/topfive/FA.TopFive.js
@@ -0,0 +1,21 @@
+!function() {
+  if(!window.FA) window.FA = {};
+  window.FA.TopFive = {
+	config: {
+          translate: {
+            en: {
+              
+            },
+            ro: {
+            }
+          }
+	},
+	
+	options: {
+          module: 3,
+          autoMod: true,
+          moduleName: 'Top Five'
+	}
+  };
+  document.write('');
+}();
