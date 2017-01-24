911a5cffdf66d965d0f4886281286c6c498b0faa
SSYT 2.0 <SSYT@users.noreply.github.com>
2017-01-24 08:34:31 +0200

    Create installer.js

---
+++ automod/installer.js
@@ -0,0 +1,9 @@
+/*
+  @automod AutoMod Installer
+  @author SSYT
+*/
+
+!function() {
+  if(!window.FA) window.FA = {};
+  if(window.FA && window.console) console.warn("Modulul FA este deja initiat."); 
+}();
