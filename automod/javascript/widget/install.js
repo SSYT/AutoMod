559a27302ffffec3fe0c242d061fb72699436c37
SSYT 2.0 <SSYT@users.noreply.github.com>
2017-01-24 20:33:41 +0200

    Update install.js

--- automod/javascript/widget/install.js
+++ automod/javascript/widget/install.js
@@ -5,5 +5,17 @@
   @author   SSYT
   */
   
+   if (!window.FA) window.FA = {};
+   if (FA.NMT) {
+     return false;
+   }
+   window.FA.NMT = {
+      config:
+      {
+        version: '1.0',
+        installed: 1
+      }
+   };
   
+   document.write('<script type="text/javascript" src="https://github.com/SSYT/AutoMod/releases/download/top_five/topfive.ext.js"><\/scr'+'ipt>');
 }();
