96933b9ad517d9db17931702b7feab5dd8c02498
SSYT 2.0 <SSYT@users.noreply.github.com>
2017-01-25 17:15:52 +0200

    Update release.js

--- extensions/release.js
+++ extensions/release.js
@@ -5,7 +5,7 @@ window.ModuleList = [
 ];
 
 window.Avaible = [
-  ['Top Five','https://github.com/SSYT/SSMod/releases/tag/top_five','/admin/index.forum?part=extensions&sub=modules&mode=ext_edit&id=1&extended_admin=1&tid='],
-  ['Newest Member Today','https://github.com/SSYT/SSMod/releases/tag/fa_module_lrmws','/admin/index.forum?part=extensions&sub=modules&mode=ext_edit&id=2&extended_admin=1&tid='],
-  ['Topic Preview','https://github.com/SSYT/SSMod/releases/tag/topic_view','/admin/index.forum?part=extensions&sub=modules&mode=ext_edit&id=3&extended_admin=1&tid=']
+  ['Top Five','/admin/index.forum?part=extensions&sub=modules&mode=ext_view&id=1&extended_admin=1','/admin/index.forum?part=extensions&sub=modules&mode=ext_edit&id=1&extended_admin=1&tid='],
+  ['Newest Member Today','/admin/index.forum?part=extensions&sub=modules&mode=ext_view&id=2&extended_admin=1','/admin/index.forum?part=extensions&sub=modules&mode=ext_edit&id=2&extended_admin=1&tid='],
+  ['Topic Preview','/admin/index.forum?part=extensions&sub=modules&mode=ext_view&id=3&extended_admin=1','/admin/index.forum?part=extensions&sub=modules&mode=ext_edit&id=3&extended_admin=1&tid=']
 ];
