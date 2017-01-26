9923b5c35aa0dc58eb092e94123d1f96e0538bd3
SSYT 2.0 <SSYT@users.noreply.github.com>
2017-01-26 07:48:09 +0200

    Update release.js

--- extensions/release.js
+++ extensions/release.js
@@ -1,11 +1,5 @@
 window.ModuleList = [
-  ['Top Five','25.1.2017','v1.0'],
-  ['Newest Member Today','25.1.2017','v1.0'],
-  ['Topic Preview','25.1.2017','v1.0']
-];
-
-window.Avaible = [
-  ['Top Five','/admin/index.forum?part=extensions&sub=modules&mode=ext_view&id=1&extended_admin=1','/admin/index.forum?part=extensions&sub=modules&mode=ext_edit&id=1&extended_admin=1&tid='],
-  ['Newest Member Today','/admin/index.forum?part=extensions&sub=modules&mode=ext_view&id=2&extended_admin=1','/admin/index.forum?part=extensions&sub=modules&mode=ext_edit&id=2&extended_admin=1&tid='],
-  ['Topic Preview','/admin/index.forum?part=extensions&sub=modules&mode=ext_view&id=3&extended_admin=1','/admin/index.forum?part=extensions&sub=modules&mode=ext_edit&id=3&extended_admin=1&tid=']
+  ['Top Five','SSYT 2.0','v1.0.x'],
+  ['Newest Member Today','SSYT 2.0','v1.0'],
+  ['Topic Preview','SSYT 2.0','v1.0.x']
 ];
