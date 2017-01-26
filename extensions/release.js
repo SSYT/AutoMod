afb14b3bc0487557b801710c7495507128022efa
SSYT 2.0 <SSYT@users.noreply.github.com>
2017-01-26 07:50:38 +0200

    Update release.js

--- extensions/release.js
+++ extensions/release.js
@@ -1,5 +1,5 @@
 window.ModuleList = [
-  ['Top Five','SSYT 2.0','v1.0.x'],
-  ['Newest Member Today','SSYT 2.0','v1.0'],
-  ['Topic Preview','SSYT 2.0','v1.0.x']
+  ['Top Five','SSYT 2.0','25.01.2017','v1.0.x'],
+  ['Newest Member Today','SSYT 2.0','25.01.2017','v1.0'],
+  ['Topic Preview','SSYT 2.0','N/A','v1.0.x']
 ];
